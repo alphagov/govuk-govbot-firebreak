857b5d98720e674e10e5b9110c0d4490e1d1af8c
Tijmen Brommet <tijmen@gmail.com>
2017-07-05 15:08:20 +0100

    Use existing app for smart answer API

--- src/smart_answer_api.js
+++ src/smart_answer_api.js
@@ -3,7 +3,7 @@ const request = require('sync-request');
 class SmartAnswerApi {
   static fetch(basePath, choices) {
     // Use local smart answer branch (has extra API stuff)
-    const url = `https://gov-bot-one.herokuapp.com/${basePath}/y/${choices.join('/')}.json`;
+    const url = `https://smart-answers-with-api.herokuapp.com/${basePath}/y/${choices.join('/')}.json`;
     console.info(`GET ${url}`);
 
     const response = request('GET', url);
