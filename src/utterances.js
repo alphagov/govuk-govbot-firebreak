f1b42add619d658dc4a6d870a94dd598df0abc09
Alec Gibson <aleckgibson@googlemail.com>
2017-07-06 11:07:48 +0100

    Allow users to re-choose

--- src/utterances.js
+++ src/utterances.js
@@ -1,17 +1,26 @@
 class Utterances {
   static matches(input, utterances) {
     return utterances.some(utterance => {
-      return new RegExp(`^${utterance}$`, 'i').test(input);
+      return new RegExp(utterance, 'i').test(input);
     });
   }
 }
 
 Utterances.stop = [
-  'stop',
-  'exit',
-  'quit',
-  'go away',
-  'shut up',
+  /^stop$/,
+  /^exit$/,
+  /^quit$/,
+  /^go away$/,
+  /^quit$/,
+];
+
+Utterances.goBack = [
+  /^back$/,
+  /^go back$/,
+  /that's not/,
+  /that is not/,
+  /not what i/,
+  /^no\b.+\w+/,
 ];
 
 module.exports = Utterances;
